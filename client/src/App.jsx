62be9eb6524a84b5ecebaddba71da60b01d723b8
Chris Mittell <mittell@hotmail.co.uk>
2022-09-24 15:30:17 +0900

    feat: added bootstrap styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,8 @@
 import './App.css';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 
 import HomePage from './pages/HomePage';
 import ProductPage from './pages/ProductPage';
@@ -7,16 +10,27 @@ import ProductPage from './pages/ProductPage';
 function App() {
 	return (
 		<BrowserRouter>
-			<div>
+			<div className='d-flex flex-column site-container'>
 				<header>
-					<Link to='/'>Amazon</Link>
+					<Navbar bg='dark' variant='dark'>
+						<Container>
+							<LinkContainer to='/'>
+								<Navbar.Brand>amazon</Navbar.Brand>
+							</LinkContainer>
+						</Container>
+					</Navbar>
 				</header>
 				<main>
-					<Routes>
-						<Route path='/product/:slug' element={<ProductPage />} />
-						<Route path='/' element={<HomePage />} />
-					</Routes>
+					<Container>
+						<Routes>
+							<Route path='/product/:slug' element={<ProductPage />} />
+							<Route path='/' element={<HomePage />} />
+						</Routes>
+					</Container>
 				</main>
+				<footer>
+					<div className='text-center'>All Rights Reserved</div>
+				</footer>
 			</div>
 		</BrowserRouter>
 	);
