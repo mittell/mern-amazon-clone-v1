d238e6f92c2486f4fb3ad9011b6c0c8570bcd62d
Chris Mittell <mittell@hotmail.co.uk>
2022-09-29 13:47:09 +0900

    feat: added User routes and logic

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
+
 import productRouter from './routes/productRoutes.js';
 import seedRouter from './routes/seedRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -18,8 +20,16 @@ mongoose
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
+
+app.use((err, req, res, next) => {
+	res.status(500).send({ message: err.message });
+});
 
 app.listen(port, () => {
 	console.log(`Server listening on Port: ${port}`);
