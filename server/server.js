5c643ee38e7b15f2944611766b7f49c1db6965d1
Chris Mittell <mittell@hotmail.co.uk>
2022-09-29 13:22:44 +0900

    feat: migrated Product route logic

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
 import data from './data.js';
+import productRouter from './routes/productRoutes.js';
 import seedRouter from './routes/seedRoutes.js';
 
 dotenv.config();
@@ -19,30 +20,9 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use('/api/seed', seedRouter);
+app.use('/api/products', productRouter);
 
-app.get('/api/products', (req, res) => {
-	res.send(data.products);
-});
 
-app.get('/api/products/:id', (req, res) => {
-	const product = data.products.find((p) => p._id === req.params.id);
-
-	if (product) {
-		res.send(product);
-	} else {
-		res.status(404).send({ message: 'Product Not Found' });
-	}
-});
-
-app.get('/api/products/slug/:slug', (req, res) => {
-	const product = data.products.find((p) => p.slug === req.params.slug);
-
-	if (product) {
-		res.send(product);
-	} else {
-		res.status(404).send({ message: 'Product Not Found' });
-	}
-});
 
 app.listen(port, () => {
 	console.log(`Server listening on Port: ${port}`);
