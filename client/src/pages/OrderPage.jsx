ec94307f078616a5690a84975ede532a75d4524b
Chris Mittell <mittell@hotmail.co.uk>
2022-10-03 13:00:56 +0900

    fix: added missing bootstrap class

--- client/src/pages/OrderPage.jsx
+++ client/src/pages/OrderPage.jsx
@@ -84,23 +84,25 @@ const OrderPage = () => {
 			<Row>
 				<Col md={8}>
 					<Card className='mb-3'>
-						<Card.Title>Shipping</Card.Title>
-						<Card.Text>
-							<strong>Name: </strong>
-							{order.shippingAddress.fullName}
-							<br />
-							<strong>Address: </strong>
-							{order.shippingAddress.address}, {order.shippingAddress.city},{' '}
-							{order.shippingAddress.postalCode},{' '}
-							{order.shippingAddress.country}
-						</Card.Text>
-						{order.isDelivered ? (
-							<MessageBox variant='success'>
-								Delivered at {order.deliveredAt}
-							</MessageBox>
-						) : (
-							<MessageBox variant='danger'>Not Delivered</MessageBox>
-						)}
+						<Card.Body>
+							<Card.Title>Shipping</Card.Title>
+							<Card.Text>
+								<strong>Name: </strong>
+								{order.shippingAddress.fullName}
+								<br />
+								<strong>Address: </strong>
+								{order.shippingAddress.address}, {order.shippingAddress.city},{' '}
+								{order.shippingAddress.postalCode},{' '}
+								{order.shippingAddress.country}
+							</Card.Text>
+							{order.isDelivered ? (
+								<MessageBox variant='success'>
+									Delivered at {order.deliveredAt}
+								</MessageBox>
+							) : (
+								<MessageBox variant='danger'>Not Delivered</MessageBox>
+							)}
+						</Card.Body>
 					</Card>
 					<Card className='mb-3'>
 						<Card.Body>
