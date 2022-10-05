d510d9310973f2df6bb111519a88a78ffb65fcac
Chris Mittell <mittell@hotmail.co.uk>
2022-10-05 09:02:16 +0900

    feat: added cors

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
-import orderRouter from './routes/orderRoutes.js';
+import cors from 'cors';
 
 import productRouter from './routes/productRoutes.js';
 import seedRouter from './routes/seedRoutes.js';
 import userRouter from './routes/userRoutes.js';
+import orderRouter from './routes/orderRoutes.js';
 
 dotenv.config();
 
@@ -24,11 +25,13 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(cors());
+
 app.get('/api/keys/paypal', (req, res) => {
 	res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
 });
 
-app.use('/api/seed', seedRouter);
+// app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
